3e96fa9d37b7a96e5ff9443de7580391aff5c095
jccgsis <jccgsis@gmail.com>
2024-07-21 01:34:14 +0100

    sleepy

--- player.py
+++ player.py
@@ -2,7 +2,7 @@ class Player:
 	def __init__(self, name, score=0):
 		self.name = name
 		self.points = score
-		self.hand = []
+		self.hand = [14]
 		self.wind = None
 
 	def add_to_hand(self, tile):
